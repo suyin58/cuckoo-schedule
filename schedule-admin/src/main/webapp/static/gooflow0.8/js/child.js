11272ad1bdea812f171b7967a6514cd8fa006af8
Silver <suzy@wjs.com>
2017-08-08 19:25:00 +0800

    gooflow base:http://127.0.0.1:8380/admin/static/gooflow0.8/demo2.html

---
+++ schedule-admin/src/main/webapp/static/gooflow0.8/js/child.js
@@ -0,0 +1,92 @@
+var jsondata = {
+		"title" : "手机上网套餐办理",
+		"nodes" : {
+			"demo_node_48" : {
+				"name" : "开始节点",
+				"left" : 200,
+				"top" : 145,
+				"type" : "node",
+				"width" : 100,
+				"height" : 24,
+				"alt" : true
+			},
+			"demo_node_49" : {
+				"name" : "中间节点1",
+				"left" : 350,
+				"top" : 147,
+				"type" : "node",
+				"width" : 100,
+				"height" : 24,
+				"alt" : true
+			},
+			"demo_node_50" : {
+				"name" : "中间节点2",
+				"left" : 511,
+				"top" : 145,
+				"type" : "node",
+				"width" : 100,
+				"height" : 24,
+				"alt" : true
+			},
+			"demo_node_51" : {
+				"name" : "结束节点",
+				"left" : 674,
+				"top" : 144,
+				"type" : "node",
+				"width" : 100,
+				"height" : 24,
+				"alt" : true
+			},
+			"demo_node_56" : {
+				"name" : "测试节点",
+				"left" : 281,
+				"top" : 239,
+				"type" : "node",
+				"width" : 100,
+				"height" : 24,
+				"alt" : true
+			}
+		},
+		"lines" : {
+			"demo_line_52" : {
+				"type" : "sl",
+				"job" : "direct",
+				"from" : "demo_node_48",
+				"to" : "demo_node_49",
+				"name" : ""
+			},
+			"demo_line_53" : {
+				"type" : "sl",
+				"job" : "direct",
+				"from" : "demo_node_49",
+				"to" : "demo_node_50",
+				"name" : ""
+			},
+			"demo_line_54" : {
+				"type" : "sl",
+				"job" : "direct",
+				"from" : "demo_node_50",
+				"to" : "demo_node_51",
+				"name" : ""
+			},
+			"demo_line_57" : {
+				"type" : "sl",
+				"job" : "direct",
+				"from" : "demo_node_48",
+				"to" : "demo_node_56",
+				"name" : "",
+				"alt" : true
+			},
+			"demo_line_58" : {
+				"type" : "tb",
+				"M": 205,
+				"job" : "dependency",
+				"from" : "demo_node_56",
+				"to" : "demo_node_49",
+				"name" : "",
+				"alt" : true
+			}
+		},
+		"areas" : {},
+		"initNum" : 60
+	}
